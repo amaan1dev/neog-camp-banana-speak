66bc6a947488ef234e0a78fde18e957b18b90b65
amaan1dev <amaan.lko32@gmail.com>
2022-09-04 03:24:29 +0530

    update: js part done

--- app.js
+++ app.js
@@ -2,9 +2,30 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-function clickHandler(){
-    outputDiv.innerHTML = "ajajajaj" + txtInput.value;
 
+var serverURL = "https://api.funtranslations.com/translate/dothraki.json"
+
+function getTranslationURL(input){
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
+
+function clickHandler(){
+    var inputText = txtInput.value; // take input 
+
+
+// calling  server to process the input url
+ fetch(getTranslationURL(inputText))
+.then(response => response.json())
+.then(json => {
+    var translatedText = json.contents.translated;
+    outputDiv.innerText = translatedText;
+})
+ .catch(errorHandler)
 };
 
-btnTranslate.addEventListener("click", clickHandler)   
+btnTranslate.addEventListener("click", clickHandler)
